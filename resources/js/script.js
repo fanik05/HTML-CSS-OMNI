854e50e19740ee61945f8a3efe8bfd67fe7fda37
fanik05 <fatinanik129@gmail.com>
2019-06-07 16:19:19 +0600

    Sticky navigation fully implemented.

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,11 @@
-/*global $, document*/
 $(document).ready(function () {
-    "use strict";
+    $('.js--section--features').waypoint(function (direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '60px'
+    });
 });
